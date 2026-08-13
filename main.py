1b33b949d0cdc391e401eaa0944142a260ca0574
giovilampa-oss <giovi.lampa@gmail.com>
2026-08-13 18:14:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ def run_web():
 # --- Configurazione Telegram ---  
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")  
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")  
-  
-def send_telegram_message(message):  
- if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:  
- print("Errore: Token o Chat ID Telegram non configurati!")  
- return  
+def send_telegram_message(message):
+    if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
+        print("Errore: Token o Chat ID Telegram non configurati!")
+        return
+
  url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"  
  payload = {  
  "chat_id": TELEGRAM_CHAT_ID,  
